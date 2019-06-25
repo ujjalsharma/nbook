e7af9315badfdd4aade9177b908315eb1d225c7f
ujjalsharma <ujjalsharma81@gmail.com>
2019-06-24 23:11:05 -0700

    initial changes

---
+++ nbook/nbook.py
@@ -0,0 +1,9 @@
+def convert(ujjal_s):
+    """
+    Print a line about converting a notebook
+    Args:
+        ujjal_s (str): person's name
+    Returns:
+        None
+    """
+    print(f"I'll convert a notebook for you someday, {ujjal_s}")
